8af08fdc0d2eaac13af4bb5fbbf37511d62f7616
Akyuna Akish <akyuna.akish@zayo.com>
2017-05-03 19:12:51 -0600

    js_modules example completed

---
+++ js_modules/src/index.js
@@ -0,0 +1,11 @@
+import {
+    add,
+    subtract,
+    multiply,
+    divide
+} from './math.js';
+
+console.log(add(1,1));
+console.log(subtract(1,1));
+console.log(multiply(10,10));
+console.log(divide(10,5));
